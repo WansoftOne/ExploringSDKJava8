54b7ae3f83208f7c154788271234683ed9ee094d
WansoftOne <wansoft.developer@gmaill.com>
2020-01-04 10:47:58 -0600

    Create Shirt class

---
+++ src/com/wansoft/entity/Shirt.java
@@ -0,0 +1,26 @@
+package com.wansoft.entity;
+
+public class Shirt {
+	private int size;
+	private String color;
+	
+	public Shirt(int size, String color) {
+		this.size = size;
+		this.color = color;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
