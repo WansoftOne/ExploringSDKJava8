b5c50cacd44cb0e5076a4d8e9758ad5c9ca3adbe
WansoftOne <wansoft.developer@gmaill.com>
2020-01-05 16:23:24 -0600

    Numerical Systems in java

---
+++ src/com/wansoft/variables/NumericalSystems.java
@@ -0,0 +1,18 @@
+package com.wansoft.variables;
+
+public class NumericalSystems {
+	
+	public static void main(String[] args) {
+		int decimalNumber = 10;
+		System.out.println("number in decimal system: " + decimalNumber);
+		
+		int octalNumber = 012;
+		System.out.println("number in octal system: " + octalNumber);
+		
+		int hexaDecimalNumber = 0xA;
+		System.out.println("number in hexadecimal system: " + hexaDecimalNumber);
+		
+		int binaryNumber = 0b1010;
+		System.out.println("number in binary system: " + binaryNumber);
+	}
+}
