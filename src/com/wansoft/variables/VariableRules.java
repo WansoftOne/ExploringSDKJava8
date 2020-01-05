a204103745dcb42bf3845c7dd2183da760c762fb
WansoftOne <wansoft.developer@gmaill.com>
2020-01-05 16:31:14 -0600

    rules for a correct identifier name

---
+++ src/com/wansoft/variables/VariableRules.java
@@ -0,0 +1,23 @@
+package com.wansoft.variables;
+
+public class VariableRules {
+	public static void main(String[] args) {
+		/**
+		 * start with lowercase
+		 */
+		String greeting = "Hello from JAVA";
+		/**
+		 * camel case
+		 */
+		String greetingFromJAva = "Hello";
+		
+		/**
+		 * valid identifiers
+		 */
+		String _name;
+		String $name;
+		String name;
+		String n123ame;
+		String name4;
+	}
+}
