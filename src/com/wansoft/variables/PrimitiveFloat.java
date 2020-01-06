522763e0703a27641221f8385dd4454f74306fee
WansoftOne <wansoft.developer@gmaill.com>
2020-01-05 21:10:11 -0600

    add information about float primitive

--- src/com/wansoft/variables/PrimitiveFloat.java
+++ src/com/wansoft/variables/PrimitiveFloat.java
@@ -3,4 +3,20 @@ package com.wansoft.variables;
 public class PrimitiveFloat {
 	private float primitiveFloat; //32 bits --default:0.0
 	private double primitiveDouble; //64 bits --default:0.0
+	
+	public static void main(String[] args) {
+		float floatVar = 100.10f;
+		System.out.println("float information");
+		System.out.println("bits in float: " + Float.SIZE);
+		System.out.println("bytes in float: " + Float.BYTES);
+		System.out.println("min value: " + Float.MIN_VALUE);
+		System.out.println("max value: " + Float.MAX_VALUE +"\n");
+		
+		double doubleVar = 10.10d;
+		System.out.println("double information");
+		System.out.println("bits in double: " + Double.SIZE);
+		System.out.println("bytes in double: " + Double.BYTES);
+		System.out.println("min value: " + Double.MIN_VALUE);
+		System.out.println("max value: " + Double.MAX_VALUE +"\n");
+	}
 }
