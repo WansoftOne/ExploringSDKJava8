708aa27788b97cf8a60c93b10935928731b845a3
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-06 11:48:49 -0600

    Create MyAnnotarion4 interface

---
+++ src/com/wansoft/annotations/MyAnnotarion4.java
@@ -0,0 +1,13 @@
+package com.wansoft.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MyAnnotarion4 {
+	public String name();
+	public String value();
+}
