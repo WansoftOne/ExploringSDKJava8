f0eb18499ae1f409b903a6a5f41aaf9ed6f74fad
WansoftOne <wansoft.developer@gmaill.com>
2020-01-05 21:10:45 -0600

    add information about boolean primitive

--- src/com/wansoft/variables/PrimitiveBoolen.java
+++ src/com/wansoft/variables/PrimitiveBoolen.java
@@ -2,4 +2,21 @@ package com.wansoft.variables;
 
 public class PrimitiveBoolen {
 	private boolean primitiveBoolean; //only contains true or false -- default:false
+	/**
+	 * boolean information
+	 */
+	public static void main(String[] args) {
+		System.out.println("first value: " + Boolean.TRUE);
+		System.out.println("second value: " + Boolean.FALSE);
+		
+		/**
+		 * creating a boolean variable
+		 */
+		boolean booleanVar = false;
+		if(booleanVar) {
+			System.out.println("is true");
+		}else {
+			System.out.println("is false");
+		}
+	}
 }
