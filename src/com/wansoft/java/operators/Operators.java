a1487b95cbe9fe15b8c0cc606f7776c5575b4bc4
Wansoft <47927541+WansoftOne@users.noreply.github.com>
2020-01-08 12:46:45 -0600

    Create Operators class

---
+++ src/com/wansoft/java/operators/Operators.java
@@ -0,0 +1,75 @@
+package com.wansoft.java.operators;
+
+public class Operators {
+	/**
+	 * Operator types
+	 * Arithmetic operators: + , - , * , / , %
+	 * Relational operators: < , > , <= , >= , != , ==
+	 * Logical operators: && ó & , || ó | , ! , ^  
+	 * Unitary operators: ~ , -
+	 * bit level operators :& , | , ^ , << , >> , >>>
+	 * assignment operators: ++ , -- , = , *= , /= , += , -= , <<= , >>= , >>>= , &= , |= , ^=
+	 * conditional operators: ?:
+	 * priority and evaluation order operators : () , [] , . , -~  	
+	 */
+	
+	/**
+	 * JAVA reserved words
+	 * abstract
+	 * boolean
+	 * break
+	 * byte
+	 * case
+	 * catch
+	 * char
+	 * clss
+	 * continue
+	 * default
+	 * null
+	 * do
+	 * double
+	 * else
+	 * extends
+	 * final
+	 * finally
+	 * float
+	 * for
+	 * if
+	 * implements
+	 * true
+	 * import
+	 * instanceof
+	 * int
+	 * interface
+	 * long
+	 * native
+	 * new 
+	 * package
+	 * private
+	 * protected
+	 * public
+	 * return
+	 * short
+	 * static
+	 * strictfp
+	 * super
+	 * switch
+	 * synchronized
+	 * this
+	 * throw
+	 * throws
+	 * transient
+	 * try
+	 * void
+	 * volatile
+	 * while
+	 * 
+	 * Palabras reservadas(Sin uso actualmente)
+	 * const goto
+	 * 
+	 * Literales Reservadas
+	 * null
+	 * true
+	 * false
+	 */
+}
